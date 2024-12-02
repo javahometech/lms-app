32d578476d063bef6f991302209f351774c58c12
Chinnu0207 <nandininandam579@gmail.com>
2024-12-02 15:54:41 +0530

    SignUp

---
+++ src/main/java/com/jm/lms/studentms/service/SignUpService.java
@@ -0,0 +1,32 @@
+
+package com.jm.lms.studentms.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jm.lms.studentms.model.SignUp;
+import com.jm.lms.studentms.repository.SignUpServiceRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignUpService {
+
+    @Autowired
+    private SignUpServiceRepository signUpServiceRepository;
+
+    public String saveRegister(SignUp signUp) {
+    	
+        if (signUpServiceRepository.existsByUsername(signUp.getUsername())) {
+            return "Username already exists."; 
+        }
+        if (signUpServiceRepository.existsByEmail(signUp.getEmail())) {
+            return "Email already exists."; 
+        }
+        signUpServiceRepository.save(signUp);
+
+        return "User registered successfully.";
+    }
+}
+
