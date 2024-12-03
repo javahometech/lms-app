882065a4236840ac2980e4d9257949a3f855459d
poojithaharinath <poojithaharinatha@gmail.com>
2024-12-03 08:35:17 +0530

    first commit of view-course-details

---
+++ src/main/java/com/jm/lms/studentms/service/CourseDetailsService.java
@@ -0,0 +1,69 @@
+package com.jm.lms.studentms.service;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
+
+import com.jm.lms.studentms.model.CourseDetails;
+import com.jm.lms.studentms.repository.CourseDetailsRepository;
+
+@Service
+public class CourseDetailsService {
+	@Autowired
+	private CourseDetailsRepository courseDetailsRepository;
+
+	public List<CourseDetails> getAllCourseDetails() {
+		return courseDetailsRepository.findAll();
+	}
+
+	public CourseDetails addCourseDetails(CourseDetails coursedetails) {
+		return courseDetailsRepository.save(coursedetails);
+	}
+
+
+	public CourseDetails updateCourseDetailsById(Long id, CourseDetails courseDetails) {
+        CourseDetails existingCourse = courseDetailsRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Course not found with id: " + id));
+         
+        existingCourse.setCourseName(courseDetails.getCourseName());
+        existingCourse.setCourseDescription(courseDetails.getCourseDescription());
+        existingCourse.setCourseInstructor(courseDetails.getCourseInstructor());
+       
+        return courseDetailsRepository.save(existingCourse);
+        
+    }
+	@SuppressWarnings("serial")
+	public class ResourceNotFoundException extends RuntimeException {
+	    public ResourceNotFoundException(String message) {
+	        super(message);
+	    }
+	}
+    
+
+	public CourseDetails updateCourseDetailsByFields(Long id, Map<String, Object> fields) {
+		 Optional<CourseDetails> existingOrder = courseDetailsRepository.findById(id);
+	        if(existingOrder.isPresent()){
+	            fields.forEach((key,val)->{
+	        Field field =  ReflectionUtils.findField(CourseDetails.class, key);
+	          field.setAccessible(true);
+	          ReflectionUtils.setField(field,existingOrder.get(),val);
+	            });
+	           return courseDetailsRepository.save(existingOrder.get());
+	        }
+	       
+
+		return null;
+	}
+	
+
+	public void deleteCourseById(Long id) {
+		courseDetailsRepository.deleteById(id);
+		
+		}
+
+	
+}
