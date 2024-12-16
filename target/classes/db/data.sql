0e6b1803f7d2be255240e241beeb82389a707449
HoneyHoney483 <164911913+HoneyHoney483@users.noreply.github.com>
2024-12-16 08:35:41 +0530

    Delete target/classes directory

--- target/classes/db/data.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO ENROLLCOURSE(id, course_name, course_username, course_password, course_duration) VALUES 
-					 (1, 'aws','Harsha_sree','Harsha@143','3 months');
-
-INSERT INTO ENROLLCOURSE(id, course_name, course_username, course_password, course_duration) VALUES 
-				 	 (2, 'java', 'Reva_sree','Revasree@143','6 months');
-				 	 
-INSERT INTO ENROLLCOURSE(id, course_name, course_username, course_password, course_duration) VALUES 
-					 (3, 'python', 'Honey', 'Honey@143','4 months');
