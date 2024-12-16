0b7fc08c766e1b9417975f3085aecd882aa3bf2a
HoneyHoney483 <164911913+HoneyHoney483@users.noreply.github.com>
2024-12-16 09:32:26 +0530

    EnrollCourse

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,17 @@
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email) VALUES 
+					 (1, 'Ashley', 'Miller', '1999-12-01', 'ashleymiller@gmail.com');
+
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email) VALUES 
+				 	 (2, 'Julia', 'Lopez', '1999-08-25', 'julialopez@gmail.com');
+				 	 
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email) VALUES 
+					 (3, 'Eric', 'Smith', '2002-03-15', 'ericsmith@gmail.com');
+
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email) VALUES 
+				 	 (4, 'Joel', 'Hernandez', '2001-10-12', 'jhernandez@gmail.com');
+				 	 
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email) VALUES 
+				 	 (5, 'Chelsey', 'Woods', '1999-06-12', 'woordsc@gmail.com');
 
 INSERT INTO ENROLLCOURSE(id, course_name, course_username, course_password, course_duration) VALUES 
 					 (1, 'aws','Harsha_sree','Harsha@143','3 months');
