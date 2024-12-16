5bb4334892a384c570a61ed747d539e0159120f5
Chinnu0207 <144435597+Chinnu0207@users.noreply.github.com>
2024-12-16 08:47:52 +0530

    Update schema.sql

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -6,3 +6,10 @@ create table students (
 	email varchar(254) not null,
 	primary key (id)
 );
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
