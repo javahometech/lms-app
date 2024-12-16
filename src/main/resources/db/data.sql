ea87355db8c54dfaf2f494f9d637c787df4d1212
Chinnu0207 <144435597+Chinnu0207@users.noreply.github.com>
2024-12-16 08:47:36 +0530

    Update data.sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -13,14 +13,12 @@ INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email) VALUES
 INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email) VALUES 
 				 	 (5, 'Chelsey', 'Woods', '1999-06-12', 'woordsc@gmail.com');
 
-CREATE TABLE users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
-);
 
+INSERT INTO users (id, username, password, email)
+	VALUES (1, 'Nandini', 'nandhu123', 'nandininandu@gmail.com');
 
-INSERT INTO users (id, username, password, email) VALUES (1, 'Nandini', 'nandhu123', 'nandininandu@gmail.com');
-INSERT INTO users (id, username, password, email) VALUES (2, 'Ramya', 'ramya456', 'athurramya654@gmail.com');
-INSERT INTO users (id, username, password, email) VALUES (3, 'Poojitha', 'pooji789', 'bollupoojitha@gmail.com');
+INSERT INTO users (id, username, password, email) 
+	VALUES (2, 'Ramya', 'ramya456', 'athurramya654@gmail.com');
+
+INSERT INTO users (id, username, password, email) 
+	VALUES (3, 'Poojitha', 'pooji789', 'bollupoojitha@gmail.com');
