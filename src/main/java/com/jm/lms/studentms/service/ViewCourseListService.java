678e5777f896f8a3782a6f5c783f3c5a41bace39
alathoor-naveen <naveenreddynr271@gmail.com>
2024-12-19 09:57:05 +0530

    Update ViewCourseListService.java

--- src/main/java/com/jm/lms/studentms/service/ViewCourseListService.java
+++ src/main/java/com/jm/lms/studentms/service/ViewCourseListService.java
@@ -11,7 +11,7 @@ import com.jm.lms.studentms.repository.CourseViewCourseListRepository;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
-public class CourseViewCourseListService {
+public class ViewCourseListService {
 	@Autowired
 	private CourseViewCourseListRepository repository;
 
